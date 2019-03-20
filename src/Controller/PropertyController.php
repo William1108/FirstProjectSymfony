e07a4556deca46d7707cabbc87199e4f77bf2dc8
william <williamledenvic@gmail.com>
2019-03-20 11:43:45 +0100

    Prpopiétés

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyController extends AbstractController
